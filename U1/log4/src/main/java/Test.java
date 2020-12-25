ae477202d3938b73d83a92db818897d5b646be13
2313997449 <2313997449@qq.com>
2020-12-25 13:32:42 +0800

    12.24 作业

---
+++ U1/log4/src/main/java/Test.java
@@ -0,0 +1,23 @@
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Test {
+
+    private static Logger logger= LogManager.getLogger(LogManager.class);
+
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){
+            // 记录trace级别的信息
+            logger.trace("log4j2日志输出：This is trace message.");
+            // 记录debug级别的信息
+            logger.debug("log4j2日志输出：This is debug message.");
+            // 记录info级别的信息
+            logger.info("log4j2日志输出：This is info message.");
+            // 记录error级别的信息
+            logger.error("log4j2日志输出：This is error message.");
+        }
+    }
+}
